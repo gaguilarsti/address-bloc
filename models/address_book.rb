366c351c4686657061af6bde0af6323abda55119
Gama Aguilar <gama@9104studios.com>
2016-10-30 00:04:14 -0700

    Assignment 5 models: completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -14,12 +14,25 @@ class AddressBook
     entries.each do |entry|
       #10 we compare name with the name of the current entry.  if the name lexicographically proceeds entry.name, we've found the index to insert at.
       #Otherwise we increment index and continue comparing the other entries.
-      if name <entry.name
+      if name < entry.name
         break
       end
       index += 1
     end
     #11we insert a new entry into entries using the calculated index.
   entries.insert(index, Entry.new(name, phone_number, email))
+  end
+
+  def remove_entry(name, phone_number, email)
+    delete_entry = nil
+
+    @entries.each do |entry|
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email
+        delete_entry = entry
+      end
+    end
+
+    @entries.delete(delete_entry)
+
   end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -37,4 +37,23 @@ RSpec.describe AddressBook do
     end
   end
 
+  describe "#remove_entry" do
+    it "removes an entry using the name, phone_number and email address" do
+      book = AddressBook.new #why do we have to give it a new addressbook thing if we're deleting?
+      book.add_entry('Charlie Aguilar', '010.012.1817', 'charlie.aguilar@aguilar.com')
+
+      name = 'Ada Lovelace'
+      phone_number = '010.012.1815'
+      email_address = 'augusta.king@lovelace.com'
+      book.add_entry(name, phone_number, email_address)
+
+      expect(book.entries.size).to eq(2)
+      book.remove_entry(name, phone_number, email_address)
+      expect(book.entries.size).to eq(1)
+      expect(book.entries.first.name).to eq('Charlie Aguilar')
+    end
+
+
+  end
+
 end
